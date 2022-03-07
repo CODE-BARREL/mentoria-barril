33aa9c9709de863a56253296ad2f6296593daf04
Matheus da C. Gonzaga <100892879+CODE-BARREL@users.noreply.github.com>
2022-03-07 10:30:41 -0300

    Update 1.js
    Se eu tiver errado é que eu sou burraldo! Deus abençoa!!

--- exercicios/1.js
+++ exercicios/1.js
@@ -1,2 +1,30 @@
 // Definir variaveis n= 50 e m= 10
+
+var n = 50
+var m - 10
+
+console.log (Os valores de N e M são:, n && m)
+
+console.log("--------------------------")
+
 // printar o resultado da adição, subtração, divisao, multiplicacao
+
+var adi = n + m;
+var sub = n - m;
+var div = n / m;
+var mul = n * m;
+
+
+console.log (adi, "A Soma de n e m é:")
+
+console.log("--------------------------")
+
+console.log (sub, "A Substração de n e m é:")
+
+console.log("--------------------------")
+
+console.log (div, "A Divisiones de n por m é:")
+
+console.log("--------------------------")
+
+console.log (mul, "La Mulriplicacion de las variables n e m és:")
