7586333b801d812b9ad1ca7cbfd0be58e6df29b1
Matheus da C. Gonzaga <100892879+CODE-BARREL@users.noreply.github.com>
2022-03-07 14:36:35 -0300

    Update 2.js
    Code Lyoko!

--- exercicios/2.js
+++ exercicios/2.js
@@ -1,1 +1,27 @@
 // Definir uma variável idade e printar se a pessoa possui idade para beber idade >= 18
+
+var Idadedocaba = 666;
+var idadedobode = 16;
+var Idadedocordeiro = 18;
+
+console.log("---------------------");
+
+if (Idadedocaba >= 18) {
+  console.log( ˜PARABENS VOCE PODE TUMAR UMA E ME DA UM REAL˜);
+} 
+else { console.log( ˜YOU SHALL NOT DRINK˜);
+}
+
+console.log("---------------------");
+
+if (Idadedobode >= 18){
+  console.log( ˜PARABENS VOCE PODE TUMAR UMA E ME DA UM REAL˜);
+} else { console.log( ˜YOU SHALL NOT DRINK˜);
+
+console.log("---------------------");
+        
+if (Idadedocordeiro >= 18) {
+  console.log( ˜PARABENS VOCE PODE TUMAR UMA E ME DA UM REAL˜);
+} else { console.log( ˜YOU SHALL NOT DRINK˜);
+
+console.log("---------------------");
